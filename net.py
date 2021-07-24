5a718b2d4fc93f47c5c6e53a9a195869ca8c80a6
EvansVan <evansndiritu@gmail.com>
2021-07-24 05:20:52 +0300

    breakthrough

--- net.py
+++ net.py
@@ -1,10 +1,11 @@
 # os for file management
-import os, webbrowser, selenium 
+import os, webbrowser, selenium ,time
 from selenium import webdriver 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 def yFi():
     # Build tuple of (class, file) to turn in
@@ -18,36 +19,50 @@ def yFi():
 
     print(file_tup)
 
-    #using chrome to access web
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
+    try:
+        #using chrome to access web
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
 
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.get('https://192.168.100.1/')
+        driver = webdriver.Chrome(chrome_options=options)
+        driver.get('https://192.168.100.1/')
 
-    #find the username box and send value
-    driver.find_element_by_id('txt_Username').send_keys('root')
+        #find the username box and send value
+        driver.find_element_by_id('txt_Username').send_keys('root')
 
-    #find the password box and send value
-    driver.find_element_by_id('txt_Password').send_keys('@Idontlike5')
+        #find the password box and send value
+        driver.find_element_by_id('txt_Password').send_keys('@Idontlike5')
 
-    #click login button
-    driver.find_element_by_id('loginbutton').click()
+        #click login button
+        driver.find_element_by_id('loginbutton').click()
 
-    #select advanced tab
-    driver.find_element_by_id('addconfig').click()
-    #
-    #select WLAN
-    driver.find_element_by_id('wlanconfig').click()
-    #
-    ##input new password
-    table = driver.find_element_by_css_selector('wlWpaPsk')
-    #
-    #select 5g wlan
-    wlan5g = driver.find_element_by_id('wlan5basic')
-    #
-    #click apply btn
-    apply = driver.find_element_by_id('btnApplySubmit')
-    apply.click()
+        #select advanced tab
+        driver.find_element_by_id('addconfig').click()
+        
+        #select WLAN
+        driver.find_element_by_id('wlanconfig').click()
+
+        driver.find_element_by_id('wlan2basic').click()
+        
+        driver.switch_to.frame("menuIframe")
+
+        #
+        #input new password
+        driver.find_element_by_id('wlWpaPsk').clear()
+        driver.find_element_by_id('wlWpaPsk').send_keys("test")
+        
+        driver.find_element_by_id('hidewlWpaPsk').click()
+        time.sleep(10)
+        print("yes")
+        #
+        #select 5g wlan
+        #wlan5g = driver.find_element_by_id('wlan5basic')
+        #
+        #click apply btn
+        #pply = driver.find_element_by_id('btnApplySubmit')
+        #apply.click()
+
+    except NoSuchElementException as e:
+        print(e)
 
 yFi()
